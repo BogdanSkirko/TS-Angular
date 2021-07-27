fe8b080e983277931ae7a1219a7a4a617f9d2006
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-27 17:02:07 +0300

    HomeWork#2

---
+++ src/app/models/IUser.ts
@@ -0,0 +1,30 @@
+interface Icompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+interface Igeo {
+  lat: string;
+  lng: string;
+}
+
+interface Iaddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  addres: Iaddress;
+  geo: Igeo;
+  phone: string;
+  website: string;
+  company:  Icompany
+}
