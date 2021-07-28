442dba6c831f9d7d36b02f7435dbe4ad86c69569
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-28 17:05:56 +0300

    NG#homeWork#2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+// import {RouterModule} from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {UserComponent} from './components/user/user.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostComponent} from './components/post/post.component';
+import {PostsComponent} from './components/posts/posts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent,
-    UsersComponent
+    UsersComponent,
+    PostComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    // RouterModule.forRoot([
+    //   {path: 'users', component: UsersComponent},
+    //   {path: 'posts', component: PostsComponent}
+    // ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
