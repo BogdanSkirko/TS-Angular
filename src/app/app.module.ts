66f4f384452eccb40ab921880d2f7bbaa47af2a5
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-28 15:52:25 +0300

    NG#homeWork#2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
+import { UsersComponent } from './components/users/users.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule
