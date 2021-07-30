57dff6d5f912825b07d8476570b6519fc9fd2f0c
Bohdan13011991 <Bogdanskirko@gmail.com>
2021-07-30 14:48:03 +0300

    NG#2Homework

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,23 @@ import {RouterModule} from "@angular/router";
 
 import {AppComponent} from './app.component';
 import {HttpClientModule} from "@angular/common/http";
-import {UserComponent} from './components/user/user.component';
 import {UsersComponent} from './components/users/users.component';
 import {PostComponent} from './components/post/post.component';
 import {PostsComponent} from './components/posts/posts.component';
+
+import { UserComponent } from './components/user/user.component';
 import {routes} from "./routes/routes";
-import { UsersWithPostsComponent } from './components/users-with-posts/users-with-posts.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     UsersComponent,
     PostComponent,
     PostsComponent,
-    UsersWithPostsComponent
+    UserComponent,
+
   ],
   imports: [
     BrowserModule,
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,17 +1,7 @@
 import {UsersComponent} from "../components/users/users.component";
 import {PostsComponent} from "../components/posts/posts.component";
-import {UsersWithPostsComponent} from "../components/users-with-posts/users-with-posts.component";
-import {Routes} from "@angular/router";
 
-export let routes: Routes = [
-  {
-    path: 'users',
-    component: UsersComponent,
-    children: [
-      {
-        path: ':id', component: UsersWithPostsComponent}
-    ]
-  },
-  // {path: 'users/:id', component: UsersWithPostsComponent},
+export let routes = [
+  {path: 'users', component: UsersComponent},
   {path: 'posts', component: PostsComponent}
 ]
